2d70d33ae946d660c5d4401a46d3666b223a4560
Steven Sellers <sseller6@byui.edu>
2023-11-06 11:04:26 -0700

    Created wk09 folder and added 09practice

---
+++ wk09/problemSets/pointerArithmetic/230.09.CppPointer2/pointer.h
@@ -0,0 +1,22 @@
+/*************************************************************
+ * 1. Name:
+ *      -your name-
+ * 2. Assignment Name:
+ *      Practice 09: Pointer Arithmetic
+ * 3. Assignment Description:
+ *      Traverse a string using pointer notation
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, etc.
+ **************************************************************/
+
+
+/**************************************
+ * COUNT - ARRAY
+ **************************************/
+
+
+/**************************************
+ * COUNT - POINTER
+ **************************************/
