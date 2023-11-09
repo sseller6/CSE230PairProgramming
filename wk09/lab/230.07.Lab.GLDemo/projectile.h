7a3a81301121a126e8eb91381fe3433c03731892
Steven Sellers <sseller6@byui.edu>
2023-11-09 16:44:36 -0700

    started wk09 lab

---
+++ wk09/lab/230.07.Lab.GLDemo/projectile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#
+
+class Projectile
+{
+private:
+    double mass;
+    double radius;
+    vector<PositionVelocityTime> flightPath;
+public:
+};
---
+++ wk09/lab/230.07.Lab.GLDemo/testProjectile.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "projectile.h"
+#include "unitTest.h"
+
+/****************************
+* TEST PROJECTILE
+*****************************/
+
+class TestProjectile : public UnitTest
+{
+public:
+	void run()
+	{
+
+		// countArray
+		reset();
+
+		report("countArray()");
+
+		// countPointer
+		reset();
+
+		report("countPointer()");
+
+	}
+private:
+
+	/****************************
+	* TEST RESET
+	* Make sure reset method sets attributes
+	* to their default values.
+	*****************************/
+
+
+	/****************************
+	* TEST FIRE
+	*****************************/
+
+
+	/****************************
+	* TEST ADVANCE
+	*****************************/
+
+		
+
+
+};
