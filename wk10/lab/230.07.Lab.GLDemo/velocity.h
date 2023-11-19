f54d1f953ba25b05618f5d32b7b39a50571429c8
Jarom Anderson <117554581+jand19081@users.noreply.github.com>
2023-11-18 19:03:59 -0700

    restored Direction and Velocity

--- wk10/lab/230.07.Lab.GLDemo/velocity.h
+++ wk10/lab/230.07.Lab.GLDemo/velocity.h
@@ -1,9 +1,18 @@
 #pragma once
 
+using namespace std;
+// forward declaration for the Velocity unit tests
+
+class TestVelocity;
+
 class Velocity
 {
 private:
 
 public:
-
+	// Setters
+	void setDxDy(double dx, double dy);
+	// Getters
+	double getDx();
+	double getDy();
 };
